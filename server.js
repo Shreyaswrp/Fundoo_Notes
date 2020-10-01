10f57902a752de71f293c8fa924d589cbc1ddd67
Shreya Swaroop <shreyaswrp@gmail.com>
2020-10-01 21:29:57 +0530

    feat: a server file to run a web application

---
+++ server.js
@@ -0,0 +1,43 @@
+/*************************************************************
+ *
+ * Execution       : default node cmd> node server.js
+ * Purpose         : Run a nodejs server and connect to a database server
+ *
+ * @description    : Creates a app using express ,and add two body-parser middlewares 
+ *                   using express’s app.use() method. We connect to the database using mongoose.
+ *                   We run a nodejs server which listens on port number 5000. 
+ *                   
+ *
+ * @file           : server.js
+ * @overview       : Run a nodejs server and connect to a database server
+ * @module         : Fundoo_Notes
+ * @version        : 1.0
+ * @since          : 16/11/2020
+ *
+ * **********************************************************/
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+/**
+ * create express app
+ */
+const app = express();
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json())
+
+require('./config/database.config.js');
+
+require('dotenv/config');
+
+/**
+ * listen for requests
+ */
+const PORT = process.env.port || 5000;
+app.listen( PORT, () => {
+    console.log("Server is listening on port"+" " +PORT);
+});
