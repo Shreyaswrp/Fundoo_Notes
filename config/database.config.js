2e67743bc7e26366a64afe14282b8ab03d6ca719
Shreya Swaroop <shreyaswrp@gmail.com>
2020-10-06 12:55:47 +0530

    refactor: remove useCreateIndex deprecation warning

--- config/database.config.js
+++ config/database.config.js
@@ -9,7 +9,7 @@ mongoose.Promise = global.Promise;
  * Connecting to the database
  */
 const DB_CONFIG = mongoose.connect(process.env.DB_CONNECTION, {
-    useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false
+    useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, 'useCreateIndex': true
 }).then(() => {
     console.log("Successfully connected to the database");    
 }).catch(err => {
