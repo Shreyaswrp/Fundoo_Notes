c188fff92a8d7beb34040a00c7b848c38c94371f
Shreya Swaroop <shreyaswrp@gmail.com>
2020-10-10 22:24:23 +0530

    feat: bcrypt for hashing and comparing passwords

--- Utility/Utility.js
+++ Utility/Utility.js
@@ -1,6 +1,8 @@
+const bcrypt = require('bcrypt');
 const dotenv = require("dotenv");
 dotenv.config();
 const jwt = require('jsonwebtoken');
+const { callbackPromise } = require('nodemailer/lib/shared');
 
 exports.generateToken = (data) => { 
     const token = jwt.sign({data}, process.env.TOKEN_SECRET, {expiresIn: '24h'});
@@ -12,3 +14,17 @@ exports.verifyToken = (data) => {
     return decoded;
 }
 
+exports.hashPassword = (user) => {
+    const password = user.password
+    const saltRounds = 10;
+    const hashedPassword = bcrypt.hashSync(password,saltRounds);
+    return hashedPassword
+}
+
+exports.comparePasswords =  (pass1, pass2) => {
+       if(bcrypt.compareSync(pass1, pass2)){
+           return true;
+       }else{
+           return false;
+       }
+}
