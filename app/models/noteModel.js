96fe0b9d4b860bf7f8e93719a4fd0b1baf8e7a5e
Shreya Swaroop <shreyaswrp@gmail.com>
2020-10-11 22:26:51 +0530

    feat: perform crud operations for notes

---
+++ app/models/noteModel.js
@@ -0,0 +1,78 @@
+const mongoose = require('mongoose');
+
+const NoteSchema = mongoose.Schema ({
+    title: {
+        type: String,
+        required: true,
+        min: 3
+    },
+    description: {
+        type: String,
+        required: true,
+        min: 3
+    },
+    },
+    {
+        timestamps: true
+});
+
+const Note = mongoose.model('Note', NoteSchema);
+
+class NoteModel{
+
+/**
+ * Create and Save a new Note
+ */
+createNote = (data, callback) => {
+    const note = new Note({title: data.title, description: data.description});
+    note.save((err, result) => {
+        if(err){
+            callback(err, null);
+        }else{
+            callback(null, result);
+        }
+    });
+}
+
+/**
+ * Retrieve and return all notes from the database.
+ */
+findAllNotes = (data, callback) => {
+    Note.find(data, function(err,result) {
+        if(err)return callback(err,null);
+        return callback(null,result);
+    })
+}
+
+/**
+ * Find a single note with a noteId
+ */
+findOneNote = (noteId, callback) => {
+    Note.findById(noteId, function(err,data){
+        if(err)return callback(err,null);
+        return callback(null,data);
+    })
+}
+
+/**
+ * Update a note identified by the noteId in the request
+ */
+updateNote = (id, data, callback) => {
+    Note.findByIdAndUpdate(id, data,  { new: true }, function (err, post){
+    if (err) return next(err);
+    callback(null, post);
+    });    
+}
+
+/**
+ * Delete a note with the specified noteId in the request
+ */
+deleteNote = (noteId,callback) => {
+    Note.findByIdAndDelete(noteId, (err, data) => {
+    if (err || data == null) callback(err, null);
+    callback(null,"Note_deleted");  
+    })
+}
+}
+
+module.exports = new NoteModel();
