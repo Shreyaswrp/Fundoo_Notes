b64ec99fa384ec0d03bd4693e0a06d861e7a48d7
Shreya Swaroop <shreyaswrp@gmail.com>
2020-10-14 14:56:38 +0530

    refactor: userController to improve indentation

--- controller/noteController.js
+++ controller/noteController.js
@@ -14,70 +14,44 @@
  *
  * **********************************************************/
 
-const noteService = require('../service/noteService.js');
-const Joi = require("joi");
+const noteService = require('../service/noteService');
 
 class  Note{
 
-/**
-   * Function to validate request
-   * @param {*} note
-   */
-validateNote = (note) => {
-    const schema = Joi.object({
-    title: Joi.string().min(3).required(),
-    description: Joi.string().min(3).required(),
-    });
-    return schema.validate(note);
-}
-
-/**
- * @description controller to past request to create note to service
- * @params {object} data
- */
-createNote = (req, res) => {
-
-    var responseResult = {};
-
-    // Validate request
-    const { error } = this.validateNote(req.body);
-    if (error) {
-        responseResult.success = false;
-        responseResult.message = "Could not create a note";
-        responseResult.error = error;
-        res.status(422).send(responseResult)
-    }else{
-        const note = {
-            title: req.body.title,
-            description: req.body.description,
-        }
-    noteService.createNote(note, function(err, data) {
-        if (err) {
+        /**
+        * @description controller to past request to create note to service
+        * @params {object} data
+        */
+        createNote = (req, res) => {
+            var responseResult = {};
+            const noteDetails = {
+                title: req.body.title,
+                description: req.body.description,
+            }
+            noteService.createNote(noteDetails, (err, data) => {
+            if (err) {
             responseResult.success = false;
-            responseResult.error = err;
             responseResult.message = "Could not create a note";
             res.status(422).send(responseResult);
-        }else{
+        }else {
             responseResult.success = true;
             responseResult.data = data;
             responseResult.message = "Note created successfully.";
             res.status(201).send(responseResult);
         }
     });
-    }
+    
 }
 
 /**
  * @params {object} data
  * @description Retrieve and return all notes from the database.
  */
-
 findAllNotes = (req,res) => {
     var responseResult = {};
-    noteService.findAllNotes(req.body, function(err, data) {
+    noteService.findAllNotes(req.body, (err, data) => {
         if (err) {
             responseResult.success = false;
-            responseResult.error = err;
             responseResult.message = "Could not find notes";
             res.status(422).send(responseResult);
         }else{
@@ -99,13 +73,12 @@ findOneNote = (req, res) => {
     if(!regexConst.test(req.params.noteId)){
         return res.status(422).send({message: "Incorrect id.Give proper id. "});
     }
-    noteService.findOneNote(req.params.noteId, function(err, data) {
+    noteService.findOneNote(req.params.noteId, (err, data) => {
         if (err || data == null) {
             responseResult.success = false;
-            responseResult.error = err;
             responseResult.message = "Could not find a note with the given id";
             res.status(422).send(responseResult);   
-        }else{
+        }else {
             responseResult.success = true;
             responseResult.data = data;
             responseResult.message = "Note by the id provided found successfully.";
@@ -128,13 +101,12 @@ updateNote = (req, res) => {
         title: req.body.title,
         description: req.body.description
     }
-    noteService.updateNote(req.params.noteId, noteUpdate, function(err, data) {
+    noteService.updateNote(req.params.noteId, noteUpdate, (err, data) => {
         if (err) {
             responseResult.success = false;
-            responseResult.error = err;
             responseResult.message = "Could not update note with the given id";
             res.status(422).send(responseResult); 
-        }else{
+        }else {
             responseResult.success = true;
             responseResult.data = data;
             responseResult.message = "The note updated successfully.";
@@ -148,19 +120,17 @@ updateNote = (req, res) => {
  * @description Delete notes from the database.
  */
 deleteNote = (req, res) => {
-    console.log(req.params.noteId);
     var responseResult = {};
     let regexConst = new RegExp(/^[a-fA-F0-9]{24}$/);
     if(!regexConst.test(req.params.noteId)){
         return res.send({message: "Incorrect id.Give proper id. "});
     }
-    noteService.deleteNote(req.params.noteId, function(err, data) {
+    noteService.deleteNote(req.params.noteId, (err, data) => {
         if (err || data == null) {
             responseResult.success = false;
-            responseResult.error = err;
             responseResult.message = "Could not delete note with the given id";
             res.status(422).send(responseResult);
-        }else{
+        }else {
             responseResult.success = true;
             responseResult.data = data;
             responseResult.message = "Note deleted successfully ";
--- controller/userController.js
+++ controller/userController.js
@@ -3,9 +3,9 @@
  * Execution       : default node cmd> node user.controller.js
  * Purpose         : Define actions for various http methods
  *
- * @description    : Actions to be done when http methods are called. 
- *                   
- *                               
+ * @description    : Actions to be done when http methods are called.
+ *
+ *
  * @file           : userController.js
  * @overview       : Actions of http methods
  * @module         : controller
@@ -14,158 +14,173 @@
  *
  * **********************************************************/
 
-const userService = require('../service/userService');
-const utility = require('../utility/utility');
-const lib = require('../lib/sendMail');
-const validData = require('../utility/response');
+const userService = require("../service/userService");
+const utility = require("../utility/utility");
+const lib = require("../lib/sendMail");
+const logger = require("../config/logger");
 
 class UserRegistration {
-
   /**
-    * @description controller to past request to register user to service
-    * @params {object} data
-    */
-    registerUser = (req, res) => {
-        var responseResult = {};
-        if(req.body != null || req.body != undefined ) {
+   * @description controller to past request to register user to service
+   * @params {object} data
+   */
+  registerUser = (req, res) => {
+    var responseResult = {};
+    logger.info("request body" + req.body);
 
-        //validate request
-        const { error } = validData.validateUser(req.body);
-        if(error) {
-            responseResult.success = false;
-            responseResult.message = "Could not register a user";
-            res.status(422).send(responseResult)
+    if (req.body != null || req.body != undefined) {
+      //validate request
+      const { error } = utility.validateUser(req.body);
+      if (error) {
+        logger.error("error validate" + error);
+        responseResult.success = false;
+        responseResult.message = "Could not register a user";
+        res.status(422).send(responseResult);
+      }
+      const userDetails = {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        emailId: req.body.emailId,
+        password: req.body.password,
+      };
+      userService.registerUser(userDetails, (err, result) => {
+        if (err) {
+          logger.error("error" + err);
+          responseResult.success = false;
+          responseResult.message = "Could not register a user";
+          res.status(422).send(responseResult);
+        } else if (result == "user_exists") {
+          logger.error("error" + result);
+          responseResult.success = false;
+          responseResult.message = "User already exists with this email id.";
+          res.status(404).send(responseResult);
+        } else {
+          logger.info("respnse data" + result);
+          responseResult.success = true;
+          responseResult.data = result;
+          responseResult.message = "User created successfully.";
+          res.status(201).send(responseResult);
         }
-        const userDetails = {
-            firstName: req.body.firstName,
-            lastName: req.body.lastName,
-            emailId: req.body.emailId,
-            password: req.body.password
-        }
-        userService.registerUser(userDetails, (err, result) => {
-            if(err) {
-                responseResult.success = false;
-                responseResult.message = "Could not register a user";
-                res.status(422).send(responseResult) 
-            }else if(result == 'user_exists') {
-                responseResult.success = false;
-                responseResult.message = "User already exists with this email id.";
-                res.status(404).send(responseResult);
-            }
-            else {
-                responseResult.success = true;
-                responseResult.data = result;
-                responseResult.message = "User created successfully.";
-                res.status(201).send(responseResult);
-            }
-        });
-        }else {
-            responseResult.success = false;
-            responseResult.message = "Invalid Request";
-            res.status(422).send(responseResult)  
-        }
-    }    
-
-  /**
-    * @description controller to past request to login user to service
-    * @param {object} data
-    */
-    loginUser = (req, res) => {
-        var responseResult = {};
-        if(req.body != null || req.body != undefined ) {
-            const payload = {
-                emailId: req.body.emailId,
-                password: req.body.password
-            }
-        userService.loginUser(payload, (err, result) => {
-            if(err || result == null) {
-                responseResult.success = false;
-                responseResult.message = "Incorrect password or email id ! login failed.";
-                res.status(422).send(responseResult) 
-            }
-            else {
-                responseResult.success = true;
-                const token = utility.generateToken(result.emailId);
-                responseResult.token = token;
-                responseResult.message = "logged in successfully.";
-                res.status(201).send(responseResult);
-            }
-        });
-        }else {
-            responseResult.success = false;
-            responseResult.message = "Invalid Request";
-            res.status(422).send(responseResult)  
-        }
+      });
+    } else {
+      responseResult.success = false;
+      responseResult.message = "Invalid Request";
+      res.status(422).send(responseResult);
     }
+  };
 
   /**
-    * @description controller to past request to forgot password to service
-    * @param {object} data
-    */
-   forgotPassword = (req, res) => {
-        var responseResult = {};
-        if(req.body != null || req.body != undefined ) {
-            const payload = {
-                emailId: req.body.emailId
-            }
-            const token = utility.generateToken(payload);
-        userService.forgotPassword(payload, (err, result) => {
-            if(err || result == null) {
-                responseResult.success = false;
-                responseResult.message = "couldn't find email to send reset password link";
-                res.status(422).send(responseResult) 
-            }else {
-                lib.sendEmail(token, result.emailId);
-                responseResult.success = true;
-                responseResult.message = "A reset password link has been sent to your email successfully";
-                res.status(201).send(responseResult) 
-            }
-        })
-        }else {
-            responseResult.success = false;
-            responseResult.message = "Invalid Request";
-            res.status(422).send(responseResult)  
+   * @description controller to past request to login user to service
+   * @param {object} data
+   */
+  loginUser = (req, res) => {
+    var responseResult = {};
+    logger.info("request body" + req.body);
+
+    if (req.body != null || req.body != undefined) {
+      const payload = {
+        emailId: req.body.emailId,
+        password: req.body.password,
+      };
+      userService.loginUser(payload, (err, result) => {
+        if (err || result == null) {
+          logger.error("error" + err);
+          responseResult.success = false;
+          responseResult.message =
+            "Incorrect password or email id ! login failed.";
+          res.status(422).send(responseResult);
+        } else {
+          logger.info("response data" + result);
+          responseResult.success = true;
+          const token = utility.generateToken(result.emailId);
+          responseResult.token = token;
+          responseResult.message = "logged in successfully.";
+          res.status(201).send(responseResult);
         }
+      });
+    } else {
+      responseResult.success = false;
+      responseResult.message = "Invalid Request";
+      res.status(422).send(responseResult);
     }
+  };
 
   /**
-    * @description controller to past request to forgot password to service
-    * @param {object} data
-    */
-    resetPassword = (req, res) => {
-        var responseResult = {};
-        if(req.headers != null || req.headers != undefined ) {
-        let token = '';
-        let obj = '';
-        try{
-         token = utility.verifyToken(req.headers.token);
-            obj = {
-             emailId: token.data.emailId,
-             password: req.headers.password
-            }
-        }catch(err) {
-            responseResult.success = false;
-            responseResult.message = "Authentication error";
-            responseResult.error = err;
-            res.status(401).send(responseResult) 
+   * @description controller to past request to forgot password to service
+   * @param {object} data
+   */
+  forgotPassword = (req, res) => {
+    var responseResult = {};
+    logger.info("request body" + req.body);
+    if (req.body != null || req.body != undefined) {
+      const payload = {
+        emailId: req.body.emailId,
+      };
+      const token = utility.generateToken(payload);
+      userService.forgotPassword(payload, (err, result) => {
+        if (err || result == null) {
+          logger.error("error" + err);
+          responseResult.success = false;
+          responseResult.message =
+            "couldn't find email to send reset password link";
+          res.status(422).send(responseResult);
+        } else {
+          logger.info("response data" + result);
+          lib.sendEmail(token, result.emailId);
+          responseResult.success = true;
+          responseResult.message =
+            "A reset password link has been sent to your email successfully";
+          res.status(201).send(responseResult);
         }
-        userService.resetPassword(obj, (err,result) => {
-            if(err) {
-                responseResult.success = false;
-                responseResult.message = "couldn't update password";
-                res.status(422).send(responseResult)  
-            }else { 
-                responseResult.success = true;
-                responseResult.message = "Password updated successfully";
-                res.status(201).send(responseResult) 
-            }
-        });
-        }else {
-                responseResult.success = false;
-                responseResult.message = "Authentication error! Request body not found";
-                res.status(422).send(responseResult)
-        }
+      });
+    } else {
+      responseResult.success = false;
+      responseResult.message = "Invalid Request";
+      res.status(422).send(responseResult);
     }
+  };
+
+  /**
+   * @description controller to past request to forgot password to service
+   * @param {object} data
+   */
+  resetPassword = (req, res) => {
+    var responseResult = {};
+    logger.info("request body" + req.body);
+    if (req.headers != null || req.headers != undefined) {
+      let token = "";
+      let obj = "";
+      try {
+        token = utility.verifyToken(req.headers.token);
+        obj = {
+          emailId: token.data.emailId,
+          password: req.headers.password,
+        };
+      } catch (err) {
+        logger.error("error" + err);
+        responseResult.success = false;
+        responseResult.message = "Authentication error";
+        res.status(401).send(responseResult);
+      }
+      userService.resetPassword(obj, (err, result) => {
+        if (err) {
+          logger.error("error" + err);
+          responseResult.success = false;
+          responseResult.message = "couldn't update password";
+          res.status(422).send(responseResult);
+        } else {
+          logger.info("response data" + result);
+          responseResult.success = true;
+          responseResult.message = "Password updated successfully";
+          res.status(201).send(responseResult);
+        }
+      });
+    } else {
+      responseResult.success = false;
+      responseResult.message = "Authentication error! Request body not found";
+      res.status(422).send(responseResult);
+    }
+  };
 }
 
 module.exports = new UserRegistration();
