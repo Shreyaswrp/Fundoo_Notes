25512e35ab6313c0b05fca0a09a65d6225964359
Shreya Swaroop <shreyaswrp@gmail.com>
2020-10-14 16:35:04 +0530

    refactor: noteService to improve indentation

--- service/noteService.js
+++ service/noteService.js
@@ -1,50 +1,50 @@
-const Note = require('../app/models/noteModel.js');
+const Note = require("../app/models/noteModel.js");
 
-class NoteService{
-    /**
-     * @params {object} data
-     * @params {callback function} callback
-     * @description create a note
-     */
-    createNote = (data,callback) => {
-       return Note.createNote(data, callback);
-    }
-    
-    /**
-     * @params {object} data
-     * @params {callback function} callback
-     * @description find all notes
-     */
-    findAllNotes = (data,callback) => {
-        return Note.findAllNotes(data, callback)
-    }
-    
-    /**
-     * @params {object} data
-     * @params {callback function} callback
-     * @description find a note
-     */
-    findOneNote = (data, callback) => {
-        return Note.findOneNote(data, callback)
-    }
-    
-    /**
-     * @params {object} data
-     * @params {callback function} callback
-     * @description update a note
-     */
-    updateNote = (id, data, callback) => {
-        return Note.updateNote(id, data, callback);
-    }
-    
-    /**
-     * @params {object} data
-     * @params {callback function} callback
-     * @description delete a note
-     */
-    deleteNote = (data, callback) => {
-        return Note.deleteNote(data, callback)
-    }
- }
-    
+class NoteService {
+  /**
+   * @params {object} data
+   * @params {callback function} callback
+   * @description create a note
+   */
+  createNote = (data, callback) => {
+    return Note.createNote(data, callback);
+  };
+
+  /**
+   * @params {object} data
+   * @params {callback function} callback
+   * @description find all notes
+   */
+  findAllNotes = (data, callback) => {
+    return Note.findAllNotes(data, callback);
+  };
+
+  /**
+   * @params {object} data
+   * @params {callback function} callback
+   * @description find a note
+   */
+  findOneNote = (data, callback) => {
+    return Note.findOneNote(data, callback);
+  };
+
+  /**
+   * @params {object} data
+   * @params {callback function} callback
+   * @description update a note
+   */
+  updateNote = (id, data, callback) => {
+    return Note.updateNote(id, data, callback);
+  };
+
+  /**
+   * @params {object} data
+   * @params {callback function} callback
+   * @description delete a note
+   */
+  deleteNote = (data, callback) => {
+    return Note.deleteNote(data, callback);
+  };
+}
+
 module.exports = new NoteService();
