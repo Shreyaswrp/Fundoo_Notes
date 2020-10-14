65fdefcc49597f2564cb91c8ad1e779977bd3bdd
Shreya Swaroop <shreyaswrp@gmail.com>
2020-10-14 16:30:16 +0530

    refactor: noteModel to improve indentation

--- app/models/noteModel.js
+++ app/models/noteModel.js
@@ -1,103 +1,95 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const NoteSchema = mongoose.Schema ({
+const NoteSchema = mongoose.Schema(
+  {
     title: {
-        type: String,
+      type: String,
+      required: true,
     },
     description: {
-        type: String,
+      type: String,
     },
     reminder: {
-        type: String,
-    },
-    collaborator: {
-        type: String,
-        default: '',
+      type: String,
     },
     colour: {
-        type: String,
-        default: '',
+      type: String,
+      default: "Green",
     },
     image: {
-        type: String,
-        default: '',
-    },
-    addLabel: {
-        type: String,
-        default: '',
-    },
-    deleteNote: {
-        type: String,
-        default: '',
+      type: String,
     },
     isPinned: {
-        type: Boolean,
-        default: false,
+      type: Boolean,
+      default: false,
     },
     isArchived: {
-        type: Boolean,
-        default: false,
+      type: Boolean,
+      default: false,
     },
-    },
-    {
-        timestamps: true
-});
+  },
+  {
+    timestamps: true,
+  }
+);
 
-const Note = mongoose.model('Note', NoteSchema);
+const Note = mongoose.model("Note", NoteSchema);
 
 class NoteModel {
+  /**
+   * @params {object} data
+   * @params {callback function} callback
+   * @description create a new note in the database
+   */
+  createNote = (data, callback) => {
+    const noteData = new Note({
+      title: data.title,
+      description: data.description,
+    });
+    return noteData.save(callback);
+  };
 
-    /**
-    * @params {object} data
-    * @params {callback function} callback
-    * @description create a new note in the database
-    */
-    createNote = (data, callback) => {
-        const noteData = new Note({title: data.title, description: data.description});
-            return noteData.save(callback);
-    }
+  /**
+   * @params {object} data
+   * @params {callback function} callback
+   * @description Retrieve and return all notes from the database.
+   */
+  findAllNotes = (data, callback) => {
+    return Note.find(data, callback);
+  };
 
-    /**
-    * @params {object} data
-    * @params {callback function} callback
-    * @description Retrieve and return all notes from the database.
-    */
-    findAllNotes = (data, callback) => {
-        return Note.find(data, callback)
-    }
+  /**
+   * @params {object} data
+   * @params {callback function} callback
+   * @description Find a single note with noteId
+   */
+  findOneNote = (noteId, callback) => {
+    return Note.findById(noteId, callback);
+  };
 
-    /**
-    * @params {object} data
-    * @params {callback function} callback
-    * @description Find a single note with noteId
-    */
-    findOneNote = (noteId, callback) => {
-        return Note.findById(noteId, callback)
-    }
+  /**
+   * @params {object} data
+   * @params {callback function} callback
+   * @description Update a note identified by the noteId in the request
+   */
+  updateNote = (id, data, callback) => {
+    return Note.findByIdAndUpdate(id, data, { new: true }, callback);
+  };
 
-    /**
-    * @params {object} data
-    * @params {callback function} callback
-    * @description Update a note identified by the noteId in the request
-    */
-    updateNote = (id, data, callback) => {
-        return Note.findByIdAndUpdate(id, data,  { new: true }, callback)    
-    }
-
-    /**
-    * @params {object} data
-    * @params {callback function} callback
-    * @description Delete a note with the specified noteId in the request
-    */
-    deleteNote = (noteId,callback) => {
-        Note.findByIdAndDelete(noteId, (err, data) => {
-            if (err || data == null) {
-                return callback(err, null);
-            }else {    
-                return callback(null,"Note_deleted");
-            }  
-        })
-    }
+  /**
+   * @params {object} data
+   * @params {callback function} callback
+   * @description Delete a note with the specified noteId in the request
+   */
+  deleteNote = (noteId, callback) => {
+    Note.findByIdAndDelete(noteId, (err, data) => {
+      if (err || data == null) {
+        return callback(err, null);
+      } else {
+        return callback(null, "Note_deleted");
+      }
+    });
+  };
 }
 
 module.exports = new NoteModel();
