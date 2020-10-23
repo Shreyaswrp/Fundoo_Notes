ac8ec0f0e2c05e21059d05bc6ce3bdf9f4e974e0
Shreya Swaroop <shreyaswrp@gmail.com>
2020-10-23 08:30:52 +0530

    feat: routes to perform crud on labels

--- routes/routes.js
+++ routes/routes.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const user = require('../controller/userController.js');
 const note = require('../controller/noteController.js');
 const auth = require('../middleware/authorizeUser.js');
+const cache = require('../middleware/redisCache.js');
+const label = require('../controller/labelController.js');
+const { createLabel } = require('../app/models/labelModel.js');
 
 //register a new User
 router.post('/register', user.registerUser);
@@ -23,13 +26,28 @@ router.put('/reset-password', user.resetPassword);
 router.post('/create-note', auth.authorizeUser, note.createNote);
 
 //route to find all notes
-router.get('/find-notes', auth.authorizeUser, note.findAllNotes);
+router.get('/find-notes', auth.authorizeUser, cache.getAllNotes, note.findAllNotes);
 
 //route to update a note
 router.put('/update-note/:noteId', auth.authorizeUser, note.updateNote);
 
 //route to delete a note
 router.delete('/delete-note/:noteId', auth.authorizeUser, note.deleteNote);
+
+//route to get all notes of a logged in user
+router.get('/find-user-notes', auth.authorizeUser, cache.getAllNotesOfUser, note.getUserNotes);
+
+//route to create a new label
+router.post('/create-label', auth.authorizeUser, label.createLabel);
+
+//route to find labels of a logged in user on a particular note
+router.get('/find-labels', auth.authorizeUser, label.getUserLabels);
+
+//route to update a label
+router.put('/update-label/:labelId', auth.authorizeUser, label.updateLabel);
+
+//route to delete a note
+router.delete('/delete-label/:labelId', auth.authorizeUser, label.deleteLabel);
 
 module.exports = router;
 
