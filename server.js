997d161c652d0d4e9999c50896ead4c552f1b954
Shreya Swaroop <shreyaswrp@gmail.com>
2020-10-27 13:27:06 +0530

    refactor: server file to maintain consistency in the code

--- server.js
+++ server.js
@@ -20,56 +20,23 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const routes = require('./routes/routes');
 const logger = require('./config/logger');
-require('./config/databaseConfig');
-require('dotenv/config');
+const dbConfig = require('./config/databaseConfig');
+const env = require('dotenv/config');
 const clearCache = require('./middleware/redisCache');
 const cors = require('cors');
-const swaggerUi = require('swagger-ui');
-var swaggerJSDoc = require('swagger-jsdoc');
-const swaggerDocument = require('./swagger.json');
 
 // create express app
 const app = express();
 
-// swagger definition
-var swaggerDefinition = {
-    info: {
-      title: 'Node Swagger API',
-      version: '1.0.0',
-      description: 'Demonstrating how to describe a RESTful API with Swagger',
-    },
-    host: 'localhost:5000',
-    basePath: '/',
-};
-  
-// options for the swagger docs
-var options = {
-    // import swaggerDefinitions
-    swaggerDefinition: swaggerDefinition,
-    // path to the API docs
-    apis: ['./routes/*.js'],
-};
-  
-// initialize swagger-jsdoc
-var swaggerSpec = swaggerJSDoc(options);
-  
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }))
 
 // parse requests of content-type - application/json
 app.use(bodyParser.json())
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
 app.use(cors());
 
 app.use(routes);
-
-// serve swagger
-app.get('/swagger.json', function(req, res) {
-    res.setHeader('Content-Type', 'application/json');
-    res.json(swaggerSpec);
-});
 
 //Define a simple route to display Message at the homepage
 app.get('/', (req, res) => {
